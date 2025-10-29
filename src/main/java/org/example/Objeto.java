7dca6d3c368486a7ae594d8086aad1fe2d791f05
sanherhua <santiherra06@gmail.com>
2025-10-29 14:28:11 +0100

    He añadido la clase Objeto - Santi (14:28)

---
+++ src/main/java/org/example/Objeto.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Objeto {
+    private String nombre;
+    private String descripcion;
+
+    public Objeto(String nombre, String descripcion) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+}
