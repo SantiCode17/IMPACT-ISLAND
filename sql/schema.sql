aedc3267213d7fe92c72e9074cf2df5f82a53b19
Alejandro Garcés <agarces@koibox.cloud>
2025-11-12 12:25:23 +0100

    Crear el esquema de la BBDD

---
+++ sql/schema.sql
@@ -0,0 +1,19 @@
+SET time_zone = '+00:00';
+
+CREATE TABLE IF NOT EXISTS games (
+    id INT UNSIGNED AUTO_INCREMENT,
+    name TEXT NOT NULL,
+    creation DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT games_pk PRIMARY KEY(id)
+);
+
+CONSTRAINT TABLE IF NOT EXISTS games_advancements (
+    id INT UNSIGNED AUTO_INCREMENT,
+    advancement_name TEXT NOT NULL,
+    game_id INT UNSIGNED NOT NULL,
+    obtained DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT games_advancements_pk PRIMARY KEY(id),
+    CONSTRAINT games_advancements_fk_game_id FOREIGN KEY(game_id) REFERENCES games(id)
+);
